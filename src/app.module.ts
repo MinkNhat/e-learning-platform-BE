188405f2a1d7909052702f3e665a245860a53b38
MinkNhat <nquyenminhnhat0503@gmail.com>
2026-03-31 16:09:26 +0700

    intital app

---
+++ src/app.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { softDeletePlugin } from 'soft-delete-plugin-mongoose';
+
+@Module({
+  imports: [
+    // config mongodb
+    MongooseModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+        connectionFactory: (connection) => {
+          connection.plugin(softDeletePlugin);
+          return connection;
+        }
+      }),
+      inject: [ConfigService],
+    }),
+
+    // config env
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    UsersModule,
+    AuthModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {} 
---
+++ src/main.ts
@@ -0,0 +1,44 @@
+import { NestFactory, Reflector } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { TransformInterceptor } from './core/transform.interceptor';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule); 
+  const configService = app.get(ConfigService);
+
+  // apply JwtAuthGuard globally
+  const reflector = app.get(Reflector);
+  app.useGlobalGuards(new JwtAuthGuard(reflector));
+
+  // apply transform interceptor globally
+  app.useGlobalInterceptors(new TransformInterceptor(reflector));
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
+  
+  // apply validation pipe globally
+  app.useGlobalPipes(new ValidationPipe());
+
+  // config versioning
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: ['1'],
+  });
+
+  // config CORS
+  app.enableCors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+  });
+
+  await app.listen(configService.get<string>('PORT'));
+}
+bootstrap();
