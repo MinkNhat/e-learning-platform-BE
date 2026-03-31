769a40cd9c007fcbe5e56f2e50b59bff4efdc6ec
MinkNhat <nquyenminhnhat0503@gmail.com>
2026-03-31 16:13:39 +0700

    feat: auth module with local-auth and jwt

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UsersService } from 'src/users/users.service';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './passport/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './passport/jwt.strategy';
+import ms from 'ms';
+import { AuthController } from './auth.controller';
+
+@Module({
+  imports: [
+    UsersService, 
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_ACCESS_TOKEN'),
+        signOptions: { expiresIn: ms(configService.get<string>('JWT_ACCESS_EXPIRES') as any) },
+      }),
+      inject: [ConfigService],
+    })
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  controllers: [AuthController],
+  exports: [AuthService]
+})
+export class AuthModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { IUser } from 'src/users/users.interface';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        private userService: UsersService,
+        private jwtService: JwtService
+    ) {}
+
+    async validateUser(username: string, pass: string): Promise<any> {
+        const user = await this.userService.findOneByEmail(username);
+        if (user && this.userService.isValidPassword(pass, user.password)) {
+            return user;
+        }
+        return null;
+    }
+
+    async login (user: IUser) {
+        const { _id, name, email, role } = user;
+        const payload = { 
+            sub: "token login",
+            iss: "from server",
+            _id,
+            name,
+            email,
+            role 
+        };
+        
+        return {
+            access_token: this.jwtService.sign(payload),
+            _id,
+            name,
+            email,
+            role
+        };
+    }
+}
